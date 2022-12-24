c57b13e61b11520d2525b8d6ab2b5cd733348e7d
J Phaiboon <phaiboon760@gmail.com>
2022-12-24 17:15:25 +0700

    fix some error

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ import requests
 logging.basicConfig(level=logging.INFO)
 
 
-def check_internet_connectivity(timeout=2):
+def check_internet_connectivity(timeout=0.5):
+    # Check if we can ping a known stable server
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("1.0.0.1", 53))
+        logging.info("Internet connectivity detected via DNS query to Cloudflare's DNS server.")
+        return True
+    except socket.error:
+        pass
     # Send a simple HTTP request to a web server
     try:
         response = requests.head("http://www.google.com", timeout=timeout)
@@ -19,17 +27,6 @@ def check_internet_connectivity(timeout=2):
             logging.info("Internet connectivity detected via HTTP request.")
             return True
     except (requests.ConnectionError, requests.Timeout, requests.HTTPError):
-        pass
-
-    # Check if we can ping a known stable server
-    try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(
-            ("1.1.1.1", 53))
-        logging.info(
-            "fantastic Internet connectivity detected via ping. this is mean google lost server")
-        return True
-    except socket.error:
         pass
 
     logging.warning(
