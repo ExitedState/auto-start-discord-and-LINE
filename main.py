b007a65b1c4d35d2d0b93feba8b7e4164b8b0da2
J Phaiboon <phaiboon760@gmail.com>
2022-12-24 09:25:12 +0700

    completed code V2

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import requests
 
 logging.basicConfig(level=logging.INFO)
 
+
 def check_internet_connectivity(timeout=2):
-        # Send a simple HTTP request to a web server
+    # Send a simple HTTP request to a web server
     try:
         response = requests.head("http://www.google.com", timeout=timeout)
         logging.info(f"HTTP response code: {response.status_code}")
@@ -23,14 +24,18 @@ def check_internet_connectivity(timeout=2):
     # Check if we can ping a known stable server
     try:
         socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("1.1.1.1", 53))
-        logging.info("fantastic Internet connectivity detected via ping. this is mean google lost server")
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(
+            ("1.1.1.1", 53))
+        logging.info(
+            "fantastic Internet connectivity detected via ping. this is mean google lost server")
         return True
     except socket.error:
         pass
 
-    logging.warning("No internet connectivity detected. Please connect to the internet and try again.")
+    logging.warning(
+        "No internet connectivity detected. Please connect to the internet and try again.")
     return False
+
 
 def check_if_process_running(process_name):
     for proc in psutil.process_iter():
@@ -41,6 +46,7 @@ def check_if_process_running(process_name):
             pass
     return False
 
+
 def launch_program(program_name, program_path):
     if not check_if_process_running(program_name):
         try:
@@ -48,9 +54,11 @@ def launch_program(program_name, program_path):
             logging.info(f"Opening {program_name.capitalize()}")
             time.sleep(5)
         except FileNotFoundError:
-            logging.warning(f"{program_name} could not be found. Please check your program path and try again.")
+            logging.warning(
+                f"{program_name} could not be found. Please check your program path and try again.")
             return False
     return True
+
 
 os.environ['DISCORD_PATH'] = 'C:\\Users\\phaib\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe'
 os.environ['LINE_PATH'] = 'C:\\Users\\phaib\\AppData\\Local\\LINE\\bin\\LineLauncher.exe'
@@ -59,6 +67,7 @@ approved_programs = {
     'discord': os.environ['DISCORD_PATH'],
     'line': os.environ['LINE_PATH']
 }
+
 
 def main(programs):
     while True:
@@ -71,12 +80,14 @@ def main(programs):
                         all_launched = False
                         break
                 else:
-                    logging.warning(f"{program_name} is not an approved program and will not be launched.")
+                    logging.warning(
+                        f"{program_name} is not an approved program and will not be launched.")
         if all_launched and check_if_process_running("discord") and check_if_process_running('line'):
             break
         logging.warning("Press any key to reconnect...")
         msvcrt.getch()
         os.system('cls')
+
 
 if __name__ == '__main__':
     main(approved_programs)
